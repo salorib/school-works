99169985dff0b4e909f560e8f1485fcc43b6ce4e
salorib <santi.li.correo@gmail.com>
2023-03-05 19:48:52 -0600

    renamed folder

---
+++ basic.exercises/Dollar.cpp
@@ -0,0 +1,16 @@
+# include <cstdio>
+
+main() {
+
+    float dollar, mxn;
+
+    printf("Enter total in dollars: ");
+    scanf("%f", &dollar);
+
+        mxn = dollar * 17.97;
+
+    printf("Your total in MXN is: %.2f", mxn);
+
+    return 0;
+
+}
